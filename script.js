38afa85be1b8d9416b7f71df9691f98f385328e5
Celery <anonemail123@tutanota.com>
2023-11-11 01:12:26 -0600

    n

--- script.js
+++ script.js
@@ -52,3 +52,33 @@ function dealOneCard(deck, card1, card2) {
         return { card3, message: "Card is not between" };
     }
 }
+
+// Section 2: Displaying
+
+// Function to handle dealing two cards
+function handleDealTwoCards() {
+    const result = dealTwoCards(deck);
+    if (result) {
+        drawnCards.push(result.card1, result.card2);
+        displayCard(result.card1);
+        displayCard(result.card2);
+    }
+}
+
+// Function to handle dealing one card and checking if it's between the previous two
+function handleDealOneCard() {
+    const result = dealOneCard(deck, drawnCards[0], drawnCards[1]);
+    if (result) {
+        drawnCards.push(result.card3);
+        displayCard(result.card3);
+    }
+}
+
+// Function to display a card image
+function displayCard(card) {
+    const cardDisplayArea = document.getElementById("cardDisplayArea");
+    const imageElement = document.createElement("img");
+    imageElement.src = card;
+    imageElement.alt = "Card Image";
+    cardDisplayArea.appendChild(imageElement);
+}
